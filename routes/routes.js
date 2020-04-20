d3d1cf370f2bab1c3e6814072344789c942300ca
brosoto-cmd <61399419+brosoto-cmd@users.noreply.github.com>
2020-04-20 10:22:32 +0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -13,6 +13,8 @@ const postController = require('../controllers/postController.js');
 
 const profile_viewController = require('../controllers/profile_viewController.js');
 
+const searchController = require('../controllers/searchController.js');
+
 const app = express();
 
 app.get('/', loginController.getLogin);
@@ -39,8 +41,26 @@ app.get('/view/:email/:_id', postController.getPost);
 
 app.post('/view/:email/:_id', postController.postPost);
 
-//app.post('/home/:email', homeController.postPost);
+app.get('/view_own/:email/:_id', postController.getPost);
 
-// exports the object `app` (defined above)
-// when another script exports from this file
+app.post('/view_own/:email/:_id', postController.postPost);
+
+app.get('/view_only/:email/:_id', postController.getPost);
+
+app.get('/edit_profile/:email', profileController.getEditProfile);
+
+app.post('/edit_profile/:email', profileController.editProfile);
+
+app.get('/rate/:email', profile_viewController.getRate);
+
+app.post('/rate/:email', profile_viewController.postRate);
+
+app.get('/edit_post/:_id', postController.getEditPost);
+
+app.post('/edit_post/:_id', postController.editPost);
+
+app.get('/search/:email', searchController.getSearch);
+
+app.post('/search/:email', searchController.postSearch);
+
 module.exports = app;
