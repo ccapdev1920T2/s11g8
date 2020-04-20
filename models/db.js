585f4aef4598544d227e204a53c96c9c0d3913c9
brosoto-cmd <61399419+brosoto-cmd@users.noreply.github.com>
2020-04-20 10:03:35 +0800

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,23 +1,16 @@
-
-// import module `mongoose`
 const mongoose = require('mongoose');
 
-// import module `User` from `../models/UserModel.js`
 const User = require('./UserModel.js');
 
-// ccapdev-mongoose is the name of the database
 const url = 'mongodb://localhost:27017/ccapdev-group8';
 
-// additional connection options
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true
 };
 
-// defines an object which contains necessary database functions
 const database = {
 
-    // connects to database
     connect: function () {
         mongoose.connect(url, options, function(error) {
             if(error) throw error;
@@ -25,7 +18,6 @@ const database = {
         });
     },
 
-    // inserts a single `doc` to the database based on the model `model`
     insertOne: function(model, doc) {
         model.create(doc, function(error, result) {
             if(error) throw error;
@@ -33,7 +25,6 @@ const database = {
         });
     },
 
-    // inserts multiple `docs` to the database based on the model `model`
     insertMany: function(model, docs) {
         model.insertMany(docs, function(error, result) {
             if(error) throw error;
@@ -41,10 +32,6 @@ const database = {
         });
     },
 
-    // searches for a single document based on the model `model`
-    // filtered through the object `query`
-    // limits the fields returned based on the string `projection`
-    // callback function is called after the execution of findOne() function
     findOne: function(model, query, projection, callback) {
         model.findOne(query, projection, function(error, result) {
             if(error) throw error;
@@ -52,10 +39,6 @@ const database = {
         });
     },
 
-    // searches for multiple documents based on the model `model`
-    // filtered through the object `query`
-    // limits the fields returned based on the string `projection`
-    // callback function is called after the execution of findMany() function
     findMany: function(model, query, projection, callback) {
         model.find(query, projection, function(error, result) {
             if(error) throw error;
@@ -63,9 +46,6 @@ const database = {
         });
     },
 
-    // updates the value defined in the object `update`
-    // on a single document based on the model `model`
-    // filtered by the object `filter`
     updateOne: function(model, filter, update) {
         model.updateOne(filter, update, function(error, result) {
             if(error) throw error;
@@ -73,9 +53,6 @@ const database = {
         });
     },
 
-    // updates the value defined in the object `update`
-    // on multiple documents based on the model `model`
-    // filtered using the object `filter`
     updateMany: function(model, filter, update) {
         model.updateMany(filter, update, function(error, result) {
             if(error) throw error;
@@ -83,8 +60,6 @@ const database = {
         });
     },
 
-    // deletes a single document based on the model `model`
-    // filtered using the object `conditions`
     deleteOne: function(model, conditions) {
         model.deleteOne(conditions, function (error, result) {
             if(error) throw error;
@@ -92,8 +67,6 @@ const database = {
         });
     },
 
-    // deletes multiple documents based on the model `model`
-    // filtered using the object `conditions`
     deleteMany: function(model, conditions) {
         model.deleteMany(conditions, function (error, result) {
             if(error) throw error;
@@ -103,6 +76,4 @@ const database = {
 
 }
 
-// exports the object `database` (defined above)
-// when another script exports from this file
 module.exports = database;
